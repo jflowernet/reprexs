b097763285b446c30c420fa07d1e0eefe15da5bc
Jason Flower <jflowernet@aim.com>
2023-11-16 12:37:22 +1000

    terra resample reprex

---
+++ terra_resample_area.R
@@ -0,0 +1,22 @@
+library(terra)
+
+r <- rast(nrows = 1, ncols = 3)
+values(r) <- c(3,5,7)
+r2 <- rast(nrows = 1, ncols = 2)
+
+plot(r)
+text(r, digits = 1)
+lines(as.polygons(r2), col = "red")
+
+r_resample <- resample(r, r2, method = "average")
+plot(r_resample)
+text(r_resample, digits = 1)
+
+#this doesn't agree with my manual calculation following the gdal Github issue: https://github.com/OSGeo/gdal/pull/2363
+
+#this is area weighted average: 
+#left hand pixel
+(3*1 + 5*0.5)/1.5
+
+#right hand pixel 
+(5*0.5 + 7*1)/1.5
