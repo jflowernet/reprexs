36600a5b7d99023b330c74662385a024a0aa98ed
Jason Flower <jflowernet@aim.com>
2023-11-16 12:50:27 +1000

    terra resample update

--- terra_resample_area.R
+++ terra_resample_area.R
@@ -8,11 +8,24 @@ plot(r)
 text(r, digits = 1)
 lines(as.polygons(r2), col = "red")
 
+r_resample <- resample(r, r2, method = "sum")
+plot(r_resample)
+text(r_resample, digits = 1)
+
+#this is agrees with manual area weighted sum: 
+#left hand pixel
+(3*1 + 5*0.5)
+
+#right hand pixel 
+(5*0.5 + 7*1)
+
+#average
+
 r_resample <- resample(r, r2, method = "average")
 plot(r_resample)
 text(r_resample, digits = 1)
 
-#this doesn't agree with my manual calculation following the gdal Github issue: https://github.com/OSGeo/gdal/pull/2363
+#this agrees with my manual calculation following the gdal Github issue: https://github.com/OSGeo/gdal/pull/2363
 
 #this is area weighted average: 
 #left hand pixel
