1fbafe7ac4ae3fda7fbbc3fce642068836609dfe
Jason Flower <jflowernet@aim.com>
2025-08-15 13:10:48 +1000

    tmap spatextent

---
+++ tmap_spatextent.R
@@ -0,0 +1,25 @@
+library(tmap)
+library(terra)
+library(sf)
+packageVersion("tmap")
+
+#Bounding box for South America
+south_america <- rnaturalearth::ne_countries(continent = "South America", returnclass = "sv")
+
+bbox_terra <- ext(south_america)
+
+bbox_sf <- st_bbox(st_as_sf(south_america))
+
+#works
+tm_shape(World, bbox = bbox_sf) + 
+  tm_polygons("HPI")
+
+#doesn't
+tm_shape(vect(World), bbox = bbox_terra) + 
+  tm_polygons("HPI")
+
+#works
+tm_shape(vect(World), bbox = st_bbox(bbox_terra)) + 
+  tm_polygons("HPI")
+
+
